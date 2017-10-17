59cc278ec2f51445c7fc975c367be51dde6639a3
旋風之音 (GoneTone) <p29022716@gmail.com>
2017-10-17 18:31:09 +0800

    Determines whether the config file is set

--- example_bot.php
+++ example_bot.php
@@ -15,8 +15,12 @@ require_once('LINEBotTiny.php');
 
 if (file_exists(__DIR__ . '/config.php')) {
     $config = include __DIR__ . '/config.php'; // 引入設定檔
-    $channelAccessToken = $config['channelAccessToken'];
-    $channelSecret = $config['channelSecret'];
+    if ($config['channelAccessToken'] == Null || $config['channelSecret'] == Null) {
+        error_log("config.php 設定檔內的驗證權杖和粉絲專頁存取權杖尚未設定完全！", 0); // 輸出錯誤
+    } else {
+        $channelAccessToken = $config['channelAccessToken'];
+        $channelSecret = $config['channelSecret'];
+    }
 } else {
     $configFile = fopen("config.php", "w") or die("Unable to open file!");
     $configFileContent = "<?php
@@ -36,7 +40,7 @@ return [
 ?>";
     fwrite($configFile, $configFileContent); // 建立文件並寫入
     fclose($configFile); // 關閉文件
-    error_log("config.php 設定檔內的驗證權杖和粉絲專頁存取權杖尚未設定！", 0); // 輸出錯誤
+    error_log("config.php 設定檔建立成功，請編輯檔案輸入驗證權杖和粉絲專頁存取權杖！", 0); // 輸出錯誤
 }
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
