78ab6e9985274a1b8253bd44cb0bbc588ec383f2
GoneTone <p29022716@gmail.com>
2019-03-18 19:46:48 +0800

    Update config file

--- example_bot.php
+++ example_bot.php
@@ -13,34 +13,34 @@ date_default_timezone_set("Asia/Taipei"); // 設定時區為台北時區
 
 require_once('LINEBotTiny.php');
 
-if (file_exists(__DIR__ . '/config.php')) {
-    $config = include __DIR__ . '/config.php'; // 引入設定檔
-    if ($config['channelAccessToken'] == Null || $config['channelSecret'] == Null) {
-        error_log("config.php 設定檔內的 channelAccessToken 和 channelSecret 尚未設定完全！", 0); // 輸出錯誤
+if (file_exists(__DIR__ . '/config.ini')) {
+    $config = parse_ini_file("config.ini", true); // 解析配置檔
+    if ($config['Channel']['Token'] == Null || $config['Channel']['Secret'] == Null) {
+        error_log("config.ini 配置檔未設定完全！", 0); // 輸出錯誤
     } else {
-        $channelAccessToken = $config['channelAccessToken'];
-        $channelSecret = $config['channelSecret'];
+        $channelAccessToken = $config['Channel']['Token'];
+        $channelSecret = $config['Channel']['Secret'];
     }
 } else {
-    $configFile = fopen("config.php", "w") or die("Unable to open file!");
-    $configFileContent = "<?php
-/**
- * Copyright 2017 GoneTone
- *
- * Line Bot
- * 範例 Example Bot 配置文件
- *
- * 此範例 GitHub 專案：https://github.com/GoneTone/line-example-bot-php
- * 官方文檔：https://developers.line.biz/en/reference/messaging-api/
- */
-return [
-    'channelAccessToken' => '',
-    'channelSecret' => ''
-];
-?>";
+    $configFile = fopen("config.ini", "w") or die("Unable to open file!");
+    $configFileContent = '; Copyright 2019 GoneTone
+;
+; Line Bot
+; 範例 Example Bot 配置文件
+;
+; 此範例 GitHub 專案：https://github.com/GoneTone/line-example-bot-php
+; 官方文檔：https://developers.line.biz/en/reference/messaging-api/
+
+[Channel]
+; 請在雙引號內輸入您的 Line Bot "Channel access token"
+Token = ""
+
+; 請在雙引號內輸入您的 Line Bot "Channel secret"
+Secret = ""
+';
     fwrite($configFile, $configFileContent); // 建立文件並寫入
     fclose($configFile); // 關閉文件
-    error_log("config.php 設定檔建立成功，請編輯檔案輸入 channelAccessToken 和 channelSecret！", 0); // 輸出錯誤
+    error_log("config.ini 配置檔建立成功，請編輯檔案填入資料！", 0); // 輸出錯誤
 }
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
